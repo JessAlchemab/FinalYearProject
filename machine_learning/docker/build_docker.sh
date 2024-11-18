3c3d731f9d4c8720917a0268aa9d001417612d3a
Ubuntu <ubuntu@ip-10-1-49-13.us-west-2.compute.internal>
2024-11-18 17:21:55 +0000

    fix: fixed path for docker build. Pushed new version of notebook for predicting analysis

--- machine_learning/docker/build_docker.sh
+++ machine_learning/docker/build_docker.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 189545766043.dkr.ecr.eu-west-2.amazonaws.com
 
-docker build ./docker_gpu -t autoantibody_classifier
+docker build . -t autoantibody_classifier
 
 docker tag autoantibody_classifier 189545766043.dkr.ecr.eu-west-2.amazonaws.com/alchemab/autoantibody_classifier_gpu:latest
 docker push 189545766043.dkr.ecr.eu-west-2.amazonaws.com/alchemab/autoantibody_classifier_gpu:latest
